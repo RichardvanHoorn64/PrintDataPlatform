6343e4caa6c0fa7f485be1f9e7ff44aa30d957a8
Richard van Hoorn <admin@printdataplatform.nl>
2024-11-22 16:42:12 +0100

    22-11 test upload pdf in production

--- fileupload/storage_functions.py
+++ fileupload/storage_functions.py
@@ -12,7 +12,7 @@ def validate_pdf(file):
 def get_blob_service_client_azure():
     try:
         account_url = "https://printdatastorage.blob.core.windows.net"
-        credential = ManagedIdentityCredential
+        credential = ManagedIdentityCredential(account_url=account_url)
         return BlobServiceClient(account_url=account_url, credential=credential)
     except Exception as e:
         print('Can not connect to Blob Storage production error: ', str(e))
@@ -28,28 +28,16 @@ def get_blob_service_client_local():
 
 
 def upload_pdf_to_azure(file, blob_name, container_name):
-    # Maak een verbinding met de blob-service
     try:
-        # if DEBUG:
-        #     blob_service_client = get_blob_service_client_local()
-        # else:
-        blob_service_client = get_blob_service_client_azure()
+        account_url = "https://printdatastorage.blob.core.windows.net"
+        credential = ManagedIdentityCredential(account_url=account_url)
+        blob_service_client = BlobServiceClient(account_url=account_url, credential=credential)
+        container_client = blob_service_client.get_container_client(container_name)
+        container_client.upload_blob(name=blob_name, data=file, overwrite=True, credential=credential)
+        print(f"Bestand {blob_name} succesvol geüpload naar container {container_name}.")
     except Exception as e:
         print('Can not get blob_service_client error: ', str(e))
         raise RuntimeError(f"Can not upload pdf to Blob Storage: {e}")
 
-    # Krijg een referentie naar de container
-    try:
-        container_client = blob_service_client.get_container_client(container_name)
-    except Exception as e:
-        print('container_client error: ', str(e))
-        raise RuntimeError(f"container_client error: {e}")
-
-    # Upload het bestand
-    try:
-        container_client.upload_blob(name=blob_name, data=file, overwrite=True)
-    except Exception as e:
-        print('Upload blob error: ', str(e))
-        raise RuntimeError(f"Upload blob error: {e}")
 
 
