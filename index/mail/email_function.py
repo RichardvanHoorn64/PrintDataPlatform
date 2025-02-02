d87bfb090f453478300dd213ca659ac3a77a56a8
Richard van Hoorn <admin@printdataplatform.nl>
2025-02-02 09:17:37 +0100

    2-2-25 update 500 error mail settings

--- index/mail/email_function.py
+++ index/mail/email_function.py
@@ -1,6 +1,5 @@
 # This is a sample email Python script for PrintDataPlatform.
 from azure.communication.email import EmailClient
-# from printdataplatform.settings import AZURE_COMMUNICATION_STRING
 
 
 def send_printdataplatform_mail(subject, address, body):
@@ -8,7 +7,6 @@ def send_printdataplatform_mail(subject, address, body):
         connection_string = ("endpoint=https://printdatacommunications.europe.communication.azure.com/;accesskey"
                              "=3h3Y032ISTU5egj6Sh2hb5tTzoJHx9vxdgZUkZF8NJQrRHKSL72uJQQJ99AJACULyCpj8iiDAAAAAZCS6tjg")
 
-        # connection_string = AZURE_COMMUNICATION_STRING
         client = EmailClient.from_connection_string(connection_string)
 
         message = {
